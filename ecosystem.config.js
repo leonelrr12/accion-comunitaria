c90db8807a51194a589158bfe67586b06cb9039f
Leonel <leonellrodriguez@gmail.com>
2026-03-29 19:29:55 -0500

    ecosystem

---
+++ ecosystem.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  apps: [
+    {
+      name: "comunitaria-api",
+      script: "./server.js", // Archivo generado por output: 'standalone'
+      instances: "max", // Usa todos los núcleos disponibles
+      exec_mode: "cluster", // Modo cluster (mejor rendimiento)
+      autorestart: true,
+      watch: false,
+      max_memory_restart: "800M", // Reinicia si usa mucha memoria
+      env: {
+        NODE_ENV: "production",
+        PORT: 3000,
+      },
+      // Opcional: logs más limpios
+      log_date_format: "YYYY-MM-DD HH:mm:ss",
+      combine_logs: true,
+      error_file: "/dev/stderr",
+      out_file: "/dev/stdout",
+    },
+  ],
+};
